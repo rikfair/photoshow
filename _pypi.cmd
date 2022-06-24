04c91f1986e7ad127f84d5439421942cb6686257
rikfair <56262375+rikfair@users.noreply.github.com>
2022-06-24 09:44:53 +0100

    Updated on PyPI

--- _pypi.cmd
+++ _pypi.cmd
@@ -1,4 +1,14 @@
-cd %~dp0
+@echo off
+REM # Updated 09:17 24/06/2022
+REM # ###
+
+echo Confirm version number updated?
+pause
+
+cd %~dp0 | exit 1
+
+set HOME=.
+set PYTHONHOME=c:\python\3.7\
 
 rmdir /s /q .\dist
 
@@ -6,14 +16,31 @@ cd .\src
 forfiles /P . /M *.egg-info /C "cmd /c rmdir /s /q @file"
 cd ..
 
-c:\python\3.7\python -m pip install --upgrade build
-c:\python\3.7\python -m pip install --upgrade twine
+%PYTHONHOME%\python -m pip install --upgrade build
+%PYTHONHOME%\python -m pip install --upgrade twine
 
-c:\python\3.7\python -m build
+%PYTHONHOME%\python -m build
 
-pause Check build is correct before proceeding
-echo Username: "__token__", Password: API Key
+echo Check build is correct before proceeding
+pause
 
-c:\python\3.7\python -m twine upload --verbose dist/*
+echo Uploading to testpypi
+%PYTHONHOME%\python -m twine upload --verbose --repository testpypi dist/*
 
+echo "%PYTHONHOME%\python -m pip install --upgrade --index-url https://test.pypi.org/simple/ photoshow"
+echo Install from testpypi and confirm package is working
+pause
+
+echo Confirm all code submitted to GitHub with version
+pause
+
+echo Uploading to pypi
+%PYTHONHOME%\python -m twine upload --verbose --repository pypi dist/*
+
+echo "%PYTHONHOME%\python -m pip uninstall photoshow"
+echo "%PYTHONHOME%\python -m pip install --upgrade photoshow"
+echo Uninstall and reinstall from pypi and confirm package is working
+pause
+
+echo Completed.
 pause
